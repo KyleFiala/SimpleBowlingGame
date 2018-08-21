81202fc805535455b223b689d5a504f2e4f3b803
Kyle Fiala <kdfiala1@gmail.com>
2018-08-21 14:44:37 -0500

    Setting up some base logic.

--- SimpleBowlingGame/Class/SimpleBowlingGame.cs
+++ SimpleBowlingGame/Class/SimpleBowlingGame.cs
@@ -6,12 +6,27 @@ namespace SimpleBowlingGame.Class
 {
     class SimpleBowlingGame : ISimpleBowlingGame
     {
-        int frame;
+        private int frame;
+
+        // Default constructor initalises game to first frame
+        public SimpleBowlingGame()
+        {
+            frame = 1;
+        }
 
         public int Score { get; set; }
 
         public void RecordFrame(params int[] throws)
         {
+            // Player has two throws if it is not the 10th frame
+            if (frame < 10)
+            {
+                frame++;
+            }
+            else // Player has possible 3 throws on the 10th frame
+            {
+                frame = 1;
+            }
         }
     }
 }
