45e2223124559a0a9c04ccf26cbbcba8625b71b1
Kyle Fiala <kdfiala1@gmail.com>
2018-08-21 14:39:28 -0500

    Skeleton of Project

---
+++ SimpleBowlingGame/Class/SimpleBowlingGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleBowlingGame.Class
+{
+    class SimpleBowlingGame : ISimpleBowlingGame
+    {
+        int frame;
+
+        public int Score { get; set; }
+
+        public void RecordFrame(params int[] throws)
+        {
+        }
+    }
+}
