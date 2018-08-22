13bf1aed8bf554faa7d29a7d86a32c343295d265
Kyle Fiala <kdfiala1@gmail.com>
2018-08-21 22:44:29 -0500

    SimpleBowlingGame ready for testing.

--- SimpleBowlingGame/Class/SimpleBowlingGame.cs
+++ SimpleBowlingGame/Class/SimpleBowlingGame.cs
@@ -7,25 +7,81 @@ namespace SimpleBowlingGame.Class
     class SimpleBowlingGame : ISimpleBowlingGame
     {
         private int frame;
+        private bool strike = false;
 
-        // Default constructor initalises game to first frame
+        public int Score { get; set; }
+        // Default constructor initializes game to first frame
         public SimpleBowlingGame()
         {
             frame = 1;
         }
-
-        public int Score { get; set; }
-
         public void RecordFrame(params int[] throws)
         {
-            // Player has two throws if it is not the 10th frame
-            if (frame < 10)
+            // Store at number of pins knocked down on each throw
+            int firstThrow = throws[0];
+            int secondThrow = throws[1];
+            int frameTotal = firstThrow + secondThrow;
+            int strikeBonus = frameTotal*2;
+
+            int thirdThrow = 0;
+
+            if (throws.Length > 2 && (firstThrow+secondThrow==10))
+                thirdThrow = throws[2];
+            
+            // If input is invalid we are just going to ingore it
+            if (frameTotal > 10)
             {
-                frame++;
-            }
-            else // Player has possible 3 throws on the 10th frame
-            {
-                frame = 1;
+                if (frame < 10)
+                {
+                    // Initialize Score if it's the first frame
+                    if (frame == 1)
+                        Score = 0;
+
+                    // Check for Strike
+                    if (frameTotal >= 10)
+                    {
+                        if (strike)
+                        {
+                            Score +=  strikeBonus;
+                        }
+                        else
+                        {
+                            strike = true;
+                            Score += frameTotal;
+                        }
+                    }
+                    else // There wasn't a strike 
+                    {
+                        if (strike) // Check for strike on previous frame
+                            Score += strikeBonus;
+                        else
+                            Score += frameTotal;
+                        // Reset Strike Marker
+                        strike = false;
+                    }
+                    frame++;
+                }
+                else // 10th frames
+                {
+                    // Check for fill ball.
+                    if (frameTotal == 10)
+                    {
+                        if (strike)
+                            Score += strikeBonus + thirdThrow;
+                        else
+                            Score += frameTotal + thirdThrow;
+                    }
+                    else
+                    {
+                        if (strike)
+                            Score += strikeBonus;
+                        else
+                            Score += frameTotal;
+                    }
+                    
+                    // Prepare for the end of the game and ask if the player would like to start a new game.
+                    frame = 1;
+                }
             }
         }
     }
